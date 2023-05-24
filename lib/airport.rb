b718b01d41a00cd054f5bcb2de4a44eea7af0d26
callumahern <callum.ahern@simplybusiness.co.uk>
2023-05-24 14:13:36 +0200

    first test passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Airport
-    def land
-    
-    end
+  def land(plane)
+    @plane = plane
+  end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,16 +1,23 @@
+# frozen_string_literal: true
+
 require 'plane'
 require 'airport'
+require 'pry'
 
 RSpec.describe 'feature spec' do
-    context 'So I can get passengers to a destination' do
-        it 'instructs a plane to land at an airport and confirm that it has landed' do
-            @plane = Plane.new
-           expect(Airport.land(@plane)).eq @plane
-        end
+  context 'So I can get passengers to a destination' do
+    it 'instructs a plane to land at an airport' do
+      @plane = Plane.new
+      airport = Airport.new
+      expect(airport.land(@plane)).to eq(@plane)
     end
+
+    it 'confirm that it has landed' do
+    end
+  end
 end
 
 # As an air traffic controller
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport and confirm that it has landed
-#plane, airport, plane landing, confirmation of landing
+# plane, airport, plane landing, confirmation of landing
