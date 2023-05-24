4d4085721446f57a873b51d0d8459e368940fd85
cherelle wilson <cherelle.wilson@simplybusiness.co.uk>
2023-05-24 12:30:56 +0100

    First feature spec test

---
+++ spec/feature_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe 'feature spec' do
+    context 'So I can get passengers to a destination' do
+        it 'instructs a plane to land at an airport and confirm that it has landed' do
+            expect(1+1).to eq 2
+        end
+    end
+end
+
+# As an air traffic controller
+# So I can get passengers to a destination
+# I want to instruct a plane to land at an airport and confirm that it has landed
