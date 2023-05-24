321ece3af7baa2f0fc10c7e7e338cf3e27789936
cherelle wilson <cherelle.wilson@simplybusiness.co.uk>
2023-05-24 12:57:43 +0100

    undefined land method

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,7 +1,11 @@
+require 'plane'
+require 'airport'
+
 RSpec.describe 'feature spec' do
     context 'So I can get passengers to a destination' do
         it 'instructs a plane to land at an airport and confirm that it has landed' do
-            expect(1+1).to eq 2
+            @plane = Plane.new
+           expect(Airport.land(@plane)).eq @plane
         end
     end
 end
@@ -9,3 +13,4 @@ end
 # As an air traffic controller
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport and confirm that it has landed
+#plane, airport, plane landing, confirmation of landing
